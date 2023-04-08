a492c0501433ee37c85f87d00314b45b5195d8c3
DiositoDiego <20213tn096@utez.edu.mx>
2023-04-08 17:33:08 -0600

    actualiza el id de la institucion

--- src/main/java/mx/utez/simapi/controllers/auth/ColoresControllerAuth.java
+++ src/main/java/mx/utez/simapi/controllers/auth/ColoresControllerAuth.java
@@ -126,6 +126,7 @@ public class ColoresControllerAuth {
                 coloresToUpdate.setColorPrimario(colores.getColorPrimario());
                 coloresToUpdate.setColorSecundario(colores.getColorSecundario());
                 coloresToUpdate.setColorTerciario(colores.getColorTerciario());
+                coloresToUpdate.setIdInstitucion(colores.getIdInstitucion());
                 //guardar los cambios del registro en la base de datos
                 coloresRepository.save(coloresToUpdate);
                 response.setError(false);
